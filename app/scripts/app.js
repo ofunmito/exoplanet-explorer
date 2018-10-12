ce1c6dd29d7bf518b9cb0b167c7e2dd3ade587cc
ofunmito <funmito.odefemi@outlook.com>
2018-10-12 17:54:50 +0100

    Add forEach Solution

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,62 +11,79 @@ Instructions:
 /* jshint unused: false */
 
 (function(document) {
-  'use strict';
+    'use strict';
 
-  var home = null;
+    var home = null;
 
-  /**
-   * Helper function to show the search query.
-   * @param {String} query - The search query.
-   */
-  function addSearchHeader(query) {
-    home.innerHTML = '<h2 class="page-title">query: ' + query + '</h2>';
-  }
-
-  /**
-   * Helper function to create a planet thumbnail.
-   * @param  {Object} data - The raw data describing the planet.
-   */
-  function createPlanetThumb(data) {
-    var pT = document.createElement('planet-thumb');
-    for (var d in data) {
-      pT[d] = data[d];
-    }
-    home.appendChild(pT);
-  }
-
-  /**
-   * XHR wrapped in a promise.
-   * @param  {String} url - The URL to fetch.
-   * @return {Promise}    - A Promise that resolves when the XHR succeeds and fails otherwise.
-   */
-  function get(url) {
-    return fetch(url, {
-      method: 'get'
-    });
-  }
-
-  /**
-   * Performs an XHR for a JSON and returns a parsed JSON response.
-   * @param  {String} url - The JSON URL to fetch.
-   * @return {Promise}    - A promise that passes the parsed JSON response.
-   */
-  function getJSON(url) {
-    return get(url).then(function(response) {
-      return response.json();
-    });
-  }
-
-  window.addEventListener('WebComponentsReady', function() {
-    home = document.querySelector('section[data-route="home"]');
-    /*
-    Refactor this code!
+    /**
+     * Helper function to show the search query.
+     * @param {String} query - The search query.
      */
-    getJSON('../data/earth-like-results.json')
-    .then(function(response) {
-      response.results.forEach(function(url) {
-        getJSON(url).then(createPlanetThumb);
-      });
+    function addSearchHeader(query) {
+        home.innerHTML = '<h2 class="page-title">query: ' + query + '</h2>';
+    }
+
+    /**
+     * Helper function to create a planet thumbnail.
+     * @param  {Object} data - The raw data describing the planet.
+     */
+    function createPlanetThumb(data) {
+        var pT = document.createElement('planet-thumb');
+        for (var d in data) {
+            pT[d] = data[d];
+        }
+        home.appendChild(pT);
+    }
+
+    /**
+     * XHR wrapped in a promise.
+     * @param  {String} url - The URL to fetch.
+     * @return {Promise}    - A Promise that resolves when the XHR succeeds and fails otherwise.
+     */
+    function get(url) {
+        return fetch(url, {
+            method: 'get'
+        });
+    }
+
+    /**
+     * Performs an XHR for a JSON and returns a parsed JSON response.
+     * @param  {String} url - The JSON URL to fetch.
+     * @return {Promise}    - A promise that passes the parsed JSON response.
+     */
+    function getJSON(url) {
+        return get(url).then(function(response) {
+            return response.json();
+        });
+    }
+
+    window.addEventListener('WebComponentsReady', function() {
+        home = document.querySelector('section[data-route="home"]');
+        /*
+        Refactor this code!
+         */
+        // getJSON('../data/earth-like-results.json')
+        //     .then(function(response) {
+        //         response.results.forEach(function(url) {
+        //             getJSON(url).then(createPlanetThumb);
+        //         });
+        //     });
+        // Refactored code
+        getJSON('../data/earth-like-results.json')
+            .then(function(response) {
+                var sequence = Promise.resolve();
+
+                addSearchHeader(response.query);
+
+                response.results.forEach(function(url) {
+                    sequence = sequence.then(function() {
+                            return getJSON(url);
+                        })
+                        .then(createPlanetThumb);
+                });
+            })
+            .catch(function(e) {
+                console.log(e);
+            });
     });
-  });
 })(document);
