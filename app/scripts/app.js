ac879cc46cc1f3d4cadff65910e16dd854cb3860
Funmito <funmito.odefemi@outlook.com>
2018-10-15 11:39:01 +0100

    Add solution to bonus quiz

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,55 +9,78 @@ proper order even if all the requests haven't finished.
 /* jshint unused: false */
 
 (function(document) {
-  'use strict';
+    'use strict';
 
-  var home = null;
+    var home = null;
 
-  /**
-   * Helper function to show the search query.
-   * @param {String} query - The search query.
-   */
-  function addSearchHeader(query) {
-    home.innerHTML = '<h2 class="page-title">query: ' + query + '</h2>';
-  }
-
-  /**
-   * Helper function to create a planet thumbnail.
-   * @param  {Object} data - The raw data describing the planet.
-   */
-  function createPlanetThumb(data) {
-    var pT = document.createElement('planet-thumb');
-    for (var d in data) {
-      pT[d] = data[d];
-    }
-    home.appendChild(pT);
-  }
-
-  /**
-   * XHR wrapped in a promise
-   * @param  {String} url - The URL to fetch.
-   * @return {Promise}    - A Promise that resolves when the XHR succeeds and fails otherwise.
-   */
-  function get(url) {
-    return fetch(url);
-  }
-
-  /**
-   * Performs an XHR for a JSON and returns a parsed JSON response.
-   * @param  {String} url - The JSON URL to fetch.
-   * @return {Promise}    - A promise that passes the parsed JSON response.
-   */
-  function getJSON(url) {
-    return get(url).then(function(response) {
-      return response.json();
-    });
-  }
-
-  window.addEventListener('WebComponentsReady', function() {
-    home = document.querySelector('section[data-route="home"]');
-    /*
-    Your code goes here!
+    /**
+     * Helper function to show the search query.
+     * @param {String} query - The search query.
      */
-    // getJSON('../data/earth-like-results.json')
-  });
+    function addSearchHeader(query) {
+        home.innerHTML = '<h2 class="page-title">query: ' + query + '</h2>';
+    }
+
+    /**
+     * Helper function to create a planet thumbnail.
+     * @param  {Object} data - The raw data describing the planet.
+     */
+    function createPlanetThumb(data) {
+        var pT = document.createElement('planet-thumb');
+        for (var d in data) {
+            pT[d] = data[d];
+        }
+        home.appendChild(pT);
+    }
+
+    /**
+     * XHR wrapped in a promise
+     * @param  {String} url - The URL to fetch.
+     * @return {Promise}    - A Promise that resolves when the XHR succeeds and fails otherwise.
+     */
+    function get(url) {
+        return fetch(url);
+    }
+
+    /**
+     * Performs an XHR for a JSON and returns a parsed JSON response.
+     * @param  {String} url - The JSON URL to fetch.
+     * @return {Promise}    - A promise that passes the parsed JSON response.
+     */
+    function getJSON(url) {
+        return get(url).then(function(response) {
+            return response.json();
+        });
+    }
+
+    window.addEventListener('WebComponentsReady', function() {
+        home = document.querySelector('section[data-route="home"]');
+        /*
+        Your code goes here!
+         */
+        getJSON('../data/earth-like-results.json')
+            .then(function(response) {
+                addSearchHeader(response.query);
+                return response;
+            })
+            .then(function(response) {
+                var sequence = Promise.resolve();
+
+                // .map executes all of the network requests immediately.
+                var arrayOfExecutingPromises = response.results.map(function(result) {
+                    return getJSON(result);
+                });
+
+                arrayOfExecutingPromises.forEach(function(request) {
+                    // Loop through the pending requests that were returned by .map (and are in order) and
+                    // turn them into a sequence.
+                    // request is a getJSON() that's currently executing.
+                    sequence = sequence.then(function() {
+                        // Remember that createPlanetThumb is a Promise, so it must resolve before Promises
+                        // later in the sequence can execute.
+                        return request.then(createPlanetThumb);
+                    });
+                });
+            });
+    });
 })(document);
