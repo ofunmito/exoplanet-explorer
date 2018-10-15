73444f7da65681b3a57a141eb1a83fba29c05bcb
Funmito <funmito.odefemi@outlook.com>
2018-10-15 11:08:59 +0100

    Add solution to Promise.all quiz

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,63 +10,70 @@ Hint: you'll probably still need to use .map.
 /* jshint unused: false */
 
 (function(document) {
-  'use strict';
+    'use strict';
 
-  var home = null;
+    var home = null;
 
-  /**
-   * Helper function to show the search query.
-   * @param {String} query - The search query.
-   */
-  function addSearchHeader(query) {
-    home.innerHTML = '<h2 class="page-title">query: ' + query + '</h2>';
-  }
-
-  /**
-   * Helper function to create a planet thumbnail.
-   * @param  {Object} data - The raw data describing the planet.
-   */
-  function createPlanetThumb(data) {
-    var pT = document.createElement('planet-thumb');
-    for (var d in data) {
-      pT[d] = data[d];
-    }
-    home.appendChild(pT);
-  }
-
-  /**
-   * XHR wrapped in a promise
-   * @param  {String} url - The URL to fetch.
-   * @return {Promise}    - A Promise that resolves when the XHR succeeds and fails otherwise.
-   */
-  function get(url) {
-    return fetch(url);
-  }
-
-  /**
-   * Performs an XHR for a JSON and returns a parsed JSON response.
-   * @param  {String} url - The JSON URL to fetch.
-   * @return {Promise}    - A promise that passes the parsed JSON response.
-   */
-  function getJSON(url) {
-    return get(url).then(function(response) {
-      return response.json();
-    });
-  }
-
-  window.addEventListener('WebComponentsReady', function() {
-    home = document.querySelector('section[data-route="home"]');
-    /*
-    Refactor this code with Promise.all!
+    /**
+     * Helper function to show the search query.
+     * @param {String} query - The search query.
      */
-    getJSON('../data/earth-like-results.json')
-    .then(function(response) {
+    function addSearchHeader(query) {
+        home.innerHTML = '<h2 class="page-title">query: ' + query + '</h2>';
+    }
 
-      addSearchHeader(response.query);
+    /**
+     * Helper function to create a planet thumbnail.
+     * @param  {Object} data - The raw data describing the planet.
+     */
+    function createPlanetThumb(data) {
+        var pT = document.createElement('planet-thumb');
+        for (var d in data) {
+            pT[d] = data[d];
+        }
+        home.appendChild(pT);
+    }
 
-      response.results.map(function(url) {
-        getJSON(url).then(createPlanetThumb);
-      });
+    /**
+     * XHR wrapped in a promise
+     * @param  {String} url - The URL to fetch.
+     * @return {Promise}    - A Promise that resolves when the XHR succeeds and fails otherwise.
+     */
+    function get(url) {
+        return fetch(url);
+    }
+
+    /**
+     * Performs an XHR for a JSON and returns a parsed JSON response.
+     * @param  {String} url - The JSON URL to fetch.
+     * @return {Promise}    - A promise that passes the parsed JSON response.
+     */
+    function getJSON(url) {
+        return get(url).then(function(response) {
+            return response.json();
+        });
+    }
+
+    window.addEventListener('WebComponentsReady', function() {
+        home = document.querySelector('section[data-route="home"]');
+        /*
+        Refactor this code with Promise.all!
+         */
+        getJSON('../data/earth-like-results.json')
+            .then(function(response) {
+
+                addSearchHeader(response.query);
+
+                return Promise.all(response.results.map(getJSON));
+            })
+            .then(function(planetData) {
+                planetData.forEach(function(planet) {
+                    createPlanetThumb(planet);
+                });
+            })
+            .catch(function(error) {
+                console.log(error);
+            });
+
     });
-  });
 })(document);
